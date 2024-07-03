2af2602f03b51adc1997eddbcf5f817237166f01
MohammadNasir520 <nasirahsan520@gmail.com>
2024-07-04 04:16:15 +0600

    contact form background correction

--- src/pages/contact/ContactPageBanner.jsx
+++ src/pages/contact/ContactPageBanner.jsx
@@ -10,7 +10,7 @@ const ContactBanner = () => {
         <div
             className='contact-banner  mt-[-65px] lg:py-5  bg-black lg:px-14 '
             style={{
-                backgroundImage: 'url(/images/contactBgImg.jpg)',
+                backgroundImage: 'url(/images/contactBgImg2.jpg)',
 
                 backgroundRepeat: 'no-repeat',
                 objectFit: "cover",
@@ -19,11 +19,11 @@ const ContactBanner = () => {
         >
 
 
-            <div className='h-screen lg:flex items-center justify-center' >
+            <div className='h-screen lg:flex items-center justify-center lg:px-28' >
 
-                <div className=' lg:w-4/6  px-4 text-white '>
+                <div className=' lg:w-5/6  pl-4 text-white '>
                     <h1 className='fontFranklin  font-thin'>Feel Free To Contact Us</h1>
-                    <h1 className='bricolage-grotesque-baseTextFont  font-bold !text-5xl !leading-snug'>We're Here For <br></br>Your Solutions</h1>
+                    <h1 className='bricolage-grotesque-baseTextFont  font-bold !text-6xl !leading-snug'>We're Here For <br></br>Your Solutions</h1>
 
                     <div className='mt-7'>
                         <Button hashLink={"/contact#contactForm"}><h1 className='text-sm'>Send us a message</h1></Button>
