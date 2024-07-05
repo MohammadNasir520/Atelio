a71d33cd96538c5e36df36fc318eb775320b612d
MohammadNasir520 <nasirahsan520@gmail.com>
2024-07-05 22:43:01 +0600

    slider done

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./Reviews.css"
+import "./Reviews.css";
 
 const Reviews = () => {
     const data = [
@@ -19,50 +19,73 @@ const Reviews = () => {
         {
             name: "Jane Doe",
             designation: "CEO",
-            description: "4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora aut velit assumenda quos fuga blanditiis deleniti quisquam nobis itaque adip Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            description: "22222 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora aut velit assumenda quos fuga blanditiis deleniti quisquam nobis itaque adip Lorem ipsum dolor sit amet consectetur adipisicing elit."
         },
         {
             name: "Jane Doe",
             designation: "CEO",
-            description: "5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora aut velit assumenda quos fuga blanditiis deleniti quisquam nobis itaque adip Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            description: "22222 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora aut velit assumenda quos fuga blanditiis deleniti quisquam nobis itaque adip Lorem ipsum dolor sit amet consectetur adipisicing elit."
         },
         {
             name: "Jane Doe",
             designation: "CEO",
-            description: "6 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora aut velit assumenda quos fuga blanditiis deleniti quisquam nobis itaque adip Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            description: "22222 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora aut velit assumenda quos fuga blanditiis deleniti quisquam nobis itaque adip Lorem ipsum dolor sit amet consectetur adipisicing elit."
         },
         {
             name: "Jane Doe",
             designation: "CEO",
-            description: "7 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora aut velit assumenda quos fuga blanditiis deleniti quisquam nobis itaque adip Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            description: "22222 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora aut velit assumenda quos fuga blanditiis deleniti quisquam nobis itaque adip Lorem ipsum dolor sit amet consectetur adipisicing elit."
         },
         {
             name: "Jane Doe",
             designation: "CEO",
-            description: "8 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora aut velit assumenda quos fuga blanditiis deleniti quisquam nobis itaque adip Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            description: "22222 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora aut velit assumenda quos fuga blanditiis deleniti quisquam nobis itaque adip Lorem ipsum dolor sit amet consectetur adipisicing elit."
         },
-        {
-            name: "Jane Doe",
-            designation: "CEO",
-            description: "9 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati tempora aut velit assumenda quos fuga blanditiis deleniti quisquam nobis itaque adip Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
+        // Add more reviews as needed
     ];
 
     const settings = {
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
         arrows: false,
+        centerMode: true,
 
-        centerPadding: '0',
+        responsive: [
+            {
+                breakpoint: 1924,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    centerPadding: '0',
+                },
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     };
 
     return (
         <div
-            className='py-8 lg:pt-10 bg-black '
+            className='py-8 lg:pt-10 bg-black w-full'
             style={{
                 backgroundImage: 'url(/images/reviewBg.png)',
                 backgroundSize: '600px auto',
@@ -76,12 +99,12 @@ const Reviews = () => {
             </h1>
 
             <div className='lg:h-[600px] flex justify-center items-center'>
-                <div className='lg:w-3/4'>
+                <div className='lg:w-3/4 w-full'>
                     <Slider {...settings}>
                         {data.map((datum, index) => (
                             <div
                                 key={index}
-                                className={`py-24 ${index % 2 === 0 ? 'focus-slide' : ''}`}
+                                className={`lg:py-24 py-24`}
                             >
                                 <div className='lg:space-y-0 relative'>
                                     <div className='h-16 w-[200px] bg-white flex px-3 items-center rounded-full border-fuchsia-500 border absolute top-[-40px] left-[25px]'>
