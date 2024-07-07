9ac70e18698e4cc397a6d6683ff759f2f6a2427e
MohammadNasir520 <nasirahsan520@gmail.com>
2024-07-08 03:32:52 +0600

    [done] scroll top implement

--- src/layouyt/ManiLayout.jsx
+++ src/layouyt/ManiLayout.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
+import Scroll from '../components/Scroll/Scroll';
 
 const ManiLayout = () => {
     return (
         <div className=''>
             <Navbar></Navbar>
+            <Scroll></Scroll>
             <Outlet></Outlet>
             <Footer></Footer>
 
